edfe6aa0621b4470d91b60a36cf0b97ea062fa18
Rajufullstack04 <rajufullstack777@gmail.com>
2025-09-24 21:21:45 +0530

    typescript3

--- 8.script.js
+++ 8.script.js
@@ -1,1 +1,25 @@
-// 
+class PerminentEmployee {
+    getBonus() {
+        return 10000;
+    }
+    getPromotionName() {
+        return "Teamleder";
+    }
+    getNoticePeriod() {
+        console.log("3 months notice period");
+    }
+}
+class ContractEmployee {
+    getNoticePeriod() {
+        console.log("2 months notice period");
+    }
+}
+//i have agreed this IEmpService contract so now tell me what kind of members i have to implement??
+class FreelancerEmployee {
+    getBonus() {
+        return 0;
+    }
+    getNoticePeriod() {
+        console.log("Freelancer do't have notice period");
+    }
+}
--- 8.script.ts
+++ 8.script.ts
@@ -1,1 +1,62 @@
-// 
+// multipul interfacess and class
+ interface IBonusServce{
+    getBonus():number;
+
+ }
+
+interface IPromotionService{
+    getPromotionName():string;
+
+}
+interface IEmpNoticePeriod{
+    getNoticePeriod(): void;
+
+}
+
+interface IEmpService_1 extends IBonusServce,IEmpNoticePeriod {}
+
+
+class PerminentEmployee implements IBonusServce,IPromotionService,IEmpNoticePeriod{
+    getBonus(): number {
+        return 10000
+    }
+    getPromotionName(): string {
+        return "Teamleder"
+    }
+    getNoticePeriod(): void {
+        console.log("3 months notice period");
+    }
+
+}
+
+class ContractEmployee implements  IEmpNoticePeriod{
+    getNoticePeriod(): void {
+      console.log("2 months notice period");
+
+    }
+
+}
+
+//i have agreed this IEmpService contract so now tell me what kind of members i have to implement??
+
+class FreelancerEmployee implements IEmpService_1{
+     getBonus():number{
+        return 0;
+     }
+     getNoticePeriod(): void {
+    
+        console.log("Freelancer do't have notice period")
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
